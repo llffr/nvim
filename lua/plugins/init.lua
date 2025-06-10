224213d96e9665231e3a03354b6254ca292df86c
llffr <wilkinsonleora@gmail.com>
2025-06-09 21:55:13 -0500

    nvim-cmp to blink.cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,16 @@
 return {
-  'tpope/vim-sleuth', -- detect tabstop and shiftwidth automatically
-
+  -- Detect tabstop and shiftwidth automatically
+  'NMAC427/guess-indent.nvim',
   {
     'stevearc/oil.nvim',
     ---@module 'oil'
-    ---@type oil.setupopts
+    ---@type oil.SetupOpts
     opts = {},
-    -- optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    -- Optional dependencies
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
   },
 
   { 'folke/todo-comments.nvim', event = 'vimenter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -18,6 +20,20 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {},
+  },
+
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
     opts = {},
   },
 }
