76a427ecf59cfe02d41b9d6d699a0b1ce5a12153
llffr <wilkinsonleora@gmail.com>
2025-06-09 21:55:13 -0500

    nvim-cmp to blink.cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,11 +4,13 @@ return {
   {
     'stevearc/oil.nvim',
     ---@module 'oil'
-    ---@type oil.setupopts
+    ---@type oil.SetupOpts
     opts = {},
-    -- optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    -- Optional dependencies
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
   },
 
   { 'folke/todo-comments.nvim', event = 'vimenter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
