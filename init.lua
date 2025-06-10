7b7a405939e17b270d96ef755d51a24f36a25f2a
llffr <wilkinsonleora@gmail.com>
2025-06-09 21:55:13 -0500

    nvim-cmp to blink.cmp

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.cmd.colorscheme 'default'
 require 'options'
 require 'keymaps'
 
